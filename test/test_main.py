0746ea67b73ea88dfb9d198a12bc1f4022fc22f1
Ryan Dutton <ryan@Ryans-MBP.lan>
2018-03-27 22:57:50 +0100

    Create pull and push requests

---
+++ test/test_main.py
@@ -0,0 +1,18 @@
+import unittest
+import os
+
+from multigit.main import GitRepo
+
+
+class TestDirectory(unittest.TestCase):
+    def setUp(self):
+        self.git_repo = GitRepo()
+
+    def test_returns_current_directory(self):
+        self.assertEqual(self.git_repo.get_cwd(), os.getcwd())
+
+    def test_list_all_dirs_in_cwd(self):
+        self.assertEqual(self.git_repo.list_all(), ['test_repo', 'test_main.py'])
+
+    def test_if_dir_is_git_repo(self):
+        self.assertTrue(self.git_repo.is_git_repo('test_repo'))
