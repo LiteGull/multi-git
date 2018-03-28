97dbead2396365452bbb4d77a6bb1406cb57d8df
Ryan Dutton <ryan@Ryans-MBP.lan>
2018-03-28 22:39:46 +0100

    Added more logging

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ class TestDirectory(unittest.TestCase):
         self.assertEqual(self.git_repo.get_cwd(), os.getcwd())
 
     def test_list_all_dirs_in_cwd(self):
-        self.assertEqual(self.git_repo.list_all(), ['test_repo', 'test_main.py'])
+        self.assertEqual(self.git_repo.listdir(), ['test_repo', 'test_main.py'])
 
     def test_if_dir_is_git_repo(self):
         self.assertTrue(self.git_repo.is_git_repo('test_repo'))
