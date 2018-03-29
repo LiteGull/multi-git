2506847423c2757b7a39d8bd752da007b6d40780
LiteGull <ryandutton95@gmail.com>
2018-03-29 08:28:33 +0100

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='multigit',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Ryan Dutton',
+      url='https://github.com/LiteGull/multigit',
+      packages=['gitpython', 'src'],
+      )
